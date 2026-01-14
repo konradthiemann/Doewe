804b053eb4727f82343a141194191493ef7cb9af
Konrad Thiemann <konrad.thiemann@mdm.de>
2026-01-14 13:34:17 +0100

    manifest changes

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pQEpcZVpLaHkIK2OkP9KQBosrr99s9gRnSzgg0jwnVk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
